26fa8072cdb06d1064b57739ed471b7e6fa14a15
Andre Wagner <WagnerAndre@JohnDeere.com>
2025-10-03 09:05:57 -0500

    .

--- tests/tests.c
+++ tests/tests.c
@@ -10,34 +10,37 @@ const char* service = "tests";
 static void test_watchdog()
 {
     WatchdogProgram programs[] = {
+            { "error",   "tests/error",          NULL, 0 },
             { "infloop", "tests/infloop",        NULL, 0 },
             { "nonrec",  "tests/nonrecoverable", NULL, 0 },
-            { "error",   "tests/error",          NULL, 0 },
     };
     watchdog_init(programs, sizeof programs / sizeof programs[0]);
 
     for (size_t i = 0; i < 5; ++i)
         watchdog_step();
 
-    WatchdogProgramState infloop_state = watchdog_program_state(0);
+    WatchdogProgramState error_state = watchdog_program_state(0);
+    assert(error_state.attempts > 2);
+
+    WatchdogProgramState infloop_state = watchdog_program_state(1);
     assert(infloop_state.status == WPS_RUNNING);
     assert(infloop_state.attempts == 1);
     assert(infloop_state.pid != PID_NOT_RUNNING);
 
-    WatchdogProgramState nonrec_state = watchdog_program_state(1);
+    WatchdogProgramState nonrec_state = watchdog_program_state(2);
     assert(nonrec_state.status == WPS_GAVE_UP);
     assert(nonrec_state.attempts == 11);
     assert(nonrec_state.pid == PID_NOT_RUNNING);
-
-    WatchdogProgramState error_state = watchdog_program_state(2);
-    assert(error_state.attempts > 2);
 
     assert(os_process_still_running(infloop_state.pid, NULL));
 
     watchdog_finalize();
 
     os_sleep_ms(100);
+
+    assert(!os_process_still_running(error_state.pid, NULL));
     assert(!os_process_still_running(infloop_state.pid, NULL));
+    assert(!os_process_still_running(nonrec_state.pid, NULL));
 }
 
 int main()
