01a6ee0b76c780609b538e239bfac48565adab11
Andre Wagner <andre.nho@gmail.com>
2025-10-12 08:33:30 -0500

    .

--- src/server/ssl/ssl_server.c
+++ src/server/ssl/ssl_server.c
@@ -8,37 +8,56 @@
 #include <openssl/ssl.h>
 
 #include "../tcp/tcp_server_priv.h"
+#include "util/logs.h"
+#include "util/error.h"
 
 typedef struct SSLServer {
     TCPServer tcp_server;
     SSL_CTX*  ctx;
 } SSLServer;
 
-static void ssl_server_initialize(SSLServer* ssl_server, int port, bool open_to_world, CreateSessionF create_session_cb, size_t n_threads, SSLKey const* key)
+static int on_error(const char* str, size_t sz, void* data)
 {
+    (void) data; (void) sz;
+    ERR("ssl server: %s", str);
+    return 0;
+}
+
+
+static int ssl_server_initialize(SSLServer* ssl_server, int port, bool open_to_world, CreateSessionF create_session_cb, size_t n_threads, SSLKey const* key)
+{
+#define CHECK(v) { if (!v) { ERR_print_errors_cb(on_error, NULL); return -1; } }
     memset(ssl_server, 0, sizeof(*ssl_server));
     tcp_server_initialize((TCPServer *) ssl_server, port, open_to_world, create_session_cb, n_threads);
 
     SSL_METHOD const* method = TLS_server_method();
-    ssl_server->ctx = SSL_CTX_new(method);
-    if (!ssl_server->ctx) {
-        ERR_print_errors_fp(stderr);
-        throw NonRecoverableException("Could not initialize OpenSSL.");
+    ssl_server->ctx = SSL_CTX_new(method); CHECK(ssl_server->ctx)
+
+    // public key
+    BIO *bio = BIO_new_mem_buf(key->public_key, -1); CHECK(bio)
+    X509 *cert = PEM_read_bio_X509(bio, NULL, 0, NULL); CHECK(cert)
+    BIO_free(bio);
+    if (SSL_CTX_use_certificate(ssl_server->ctx, cert) <= 0) {
+        ERR_print_errors_cb(on_error, NULL);
+        return -1;
     }
 
-    if (SSL_CTX_use_certificate_file(ssl_server->ctx, key->public_key, SSL_FILETYPE_PEM) <= 0) {
-        ERR_print_errors_fp(stderr);
-        throw NonRecoverableException("Could not load public key.");
+    // private key
+    bio = BIO_new_mem_buf(key->private_key, -1); CHECK(bio)
+    EVP_PKEY *pkey = PEM_read_bio_PrivateKey(bio, NULL, 0, NULL); CHECK(pkey)
+    BIO_free(bio);
+    if (SSL_CTX_use_PrivateKey(ssl_server->ctx, pkey) <= 0 ) {
+        ERR_print_errors_cb(on_error, NULL);
+        return -1;
     }
-
-    if (SSL_CTX_use_PrivateKey_file(ssl_server->ctx, key->private_key, SSL_FILETYPE_PEM) <= 0 ) {
-        ERR_print_errors_fp(stderr);
-        throw NonRecoverableException("Could not load private key.");
-    }
+    
+    return 0;
+#undef CHECK
 }
 
 static void ssl_server_finalize(SSLServer* ssl_server)
 {
+    SSL_CTX_free(ssl_server->ctx);
     tcp_server_finalize((TCPServer *) ssl_server);
 }
 
@@ -49,7 +68,8 @@ SSLServer* ssl_server_create(int port, bool open_to_world, CreateSessionF create
         fprintf(stderr, "Memory exausted.\n");
         exit(EXIT_FAILURE);
     }
-    ssl_server_initialize(ssl_server, port, open_to_world, create_session_cb, n_threads);
+    if (ssl_server_initialize(ssl_server, port, open_to_world, create_session_cb, n_threads, key) < 0)
+        exit(NON_RECOVERABLE_ERROR);
     return ssl_server;
 }
 
--- src/server/ssl/ssl_server.h
+++ src/server/ssl/ssl_server.h
@@ -6,8 +6,8 @@
 typedef struct SSLServer SSLServer;
 
 typedef struct SSLKey {
-    char* public_key;
-    char* private_key;
+    const char* public_key;
+    const char* private_key;
 } SSLKey;
 
 SSLServer* ssl_server_create(int port, bool open_to_world, CreateSessionF create_session_cb, size_t n_threads, SSLKey const* key);
