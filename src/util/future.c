d982c79061c9ddd9a7de58ba8bee4aae2064078a
Andre Wagner <WagnerAndre@JohnDeere.com>
2025-10-14 09:41:52 -0500

    .

--- src/util/future.c
+++ src/util/future.c
@@ -4,11 +4,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <pthread.h>
+
 typedef struct Future {
+    void* result;
     // TODO: add fields here
 } Future;
 
-static void future_initialize(Future* future)
+static void future_initialize(Future* future, FutureThread future_thread, void* data)
 {
     memset(future, 0, sizeof(*future));
     // TODO: initialize fields here
@@ -19,14 +22,14 @@ static void future_finalize(Future* future)
     // TODO: finalize fields here
 }
 
-Future* future_create()
+Future* future_create(FutureThread future_thread, void* data)
 {
     Future* future = malloc(sizeof(Future));
     if (future == NULL) {
         fprintf(stderr, "Memory exausted.\n");
         exit(EXIT_FAILURE);
     }
-    future_initialize(future);
+    future_initialize(future, future_thread, data);
     return future;
 }
 
@@ -34,4 +37,20 @@ void future_destroy(Future* future)
 {
     future_finalize(future);
     free(future);
+}
+
+FutureStatus future_await(Future* future, void** result)
+{
+}
+
+FutureStatus future_status(Future* future)
+{
+}
+
+void* future_result(Future* future)
+{
+}
+
+void future_notify_error(Future* future, void* result)
+{
 }
--- src/util/future.h
+++ src/util/future.h
@@ -3,13 +3,17 @@
 #include <stdbool.h>
 
 typedef struct Future Future;
-typedef void*(*FutureThread)(void*);
+typedef void*(*FutureThread)(Future*, void*);
 typedef enum { FU_SUCCESS, FU_ERROR, FU_RUNNING } FutureStatus;
 
-Future* future_create(FutureThread future_thread, void* data);
-void    future_destroy(Future* future);
+Future*      future_create(FutureThread future_thread, void* data);
+void         future_destroy(Future* future);
 
 FutureStatus future_status(Future* future);
-void*        future_value(Future* future);
+void*        future_result(Future* future);
+
+FutureStatus future_await(Future* future, void** result);
+
+void         future_notify_error(Future* future, void* result);
 
 #endif
